829b75f067a1933a5918147fbf1402bb4af9b0b2
Konstantin A <agulovks@yandex.ru>
2022-03-07 18:18:55 +0300

    implement masonry layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
+import masonry from "masonry-layout"
 import isWebp from "./modules/isWebp.js"
+import imagesLoaded from "imagesloaded"
 
 isWebp()
+
+// Masonry layout
+
+let comments = document.querySelector(".testimonial__comments")
+
+imagesLoaded(comments, function (instance) {
+    new masonry(comments, {
+        columnWidth: 356,
+        itemSelector: ".comment",
+        horizontalOrder: true,
+        fitWidth: true,
+        gutter: 24,
+    })
+})
