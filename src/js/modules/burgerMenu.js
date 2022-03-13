7309dabf2d243a91a069d855a99c7554460978c2
Konstantin A <agulovks@yandex.ru>
2022-03-13 16:37:05 +0300

    add breakpoint at 900px

--- src/js/modules/burgerMenu.js
+++ src/js/modules/burgerMenu.js
@@ -3,10 +3,12 @@ let line = document.querySelector(".menu__line")
 
 export function activeMenu() {
     if (menu.classList.contains("active")) {
+        document.body.classList.remove("active")
         menu.classList.remove("active")
         menu.classList.add("not-active")
         setTimeout(() => line.classList.remove("menu__line_active"), 700)
     } else {
+        document.body.classList.add("active")
         menu.classList.remove("not-active")
         menu.classList.add("active")
         setTimeout(() => line.classList.add("menu__line_active"), 700)
